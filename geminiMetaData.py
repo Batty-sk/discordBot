b84aee91dcdd4a76dce83e0d8487ca07da3639ee
Batty-sk <saorav.skumar@gmail.com>
2025-01-21 01:32:58 +0530

    discord bot

---
+++ geminiMetaData.py
@@ -0,0 +1,16 @@
+CHAT_METADATA = f'''act as a discord bot of sk's server. the admin of the server is sk. and these are members of
+1.noob: sk's second account.
+2.gitanshu: A man who is addictive to alchol.
+3.amit: A man with bear hairs on his body.
+4.soumya: A man with expertise in full stack technologies.
+now you somehow knows the server and its member.
+
+
+**important**
+-reply in hinglish language.
+-your task is to reply to the messages ask by the members in a funny way and sarcastic way.
+-if and only if they ask about something regarding the server's info and members 
+then only reply them as per the description i have provided above otherwise answer the message in a very short description not more than 60 words.
+
+message:
+'''
---
+++ main.py
@@ -0,0 +1,82 @@
+import discord 
+import os 
+from dotenv import load_dotenv
+from geminiMetaData import CHAT_METADATA
+import google.generativeai as genai
+load_dotenv()
+
+
+
+SERVER_GENERAL_CHANNEL =''
+SERVER_GENERAL_CHANNEL_ID = 1178674808968446027
+GEMINI_TOKEN = os.environ['GEMINI_AUTHENTICATION_TOKEN']
+TOKEN = os.environ['TOKEN']
+
+#HMM CLASS BABY 
+
+
+class Gemini:
+    model = None
+    def __init__(self):
+        genai.configure(api_key=GEMINI_TOKEN)
+        self.model = genai.GenerativeModel("gemini-1.5-flash")
+
+    def chat(self,message):
+        if(self.model):
+            try:
+                response = self.model.generate_content(CHAT_METADATA+message)
+            except:
+                response =None
+            return response.text
+        return None
+
+
+async def which_command(message,author):
+    command = message.split(" ")
+    if(not len(command)>1):
+        return
+    message=""
+    for i in range(len(command)):
+        if(i>1):
+            message+=command[i];
+        
+    print("message",message)
+    if(command[1] == "/chat"):
+        response = gemini.chat(message=message)
+        if(response):
+            await SERVER_GENERAL_CHANNEL.send(response)
+        
+client = discord.Client(intents=discord.Intents.default())
+gemini = Gemini()
+
+
+
+
+@client.event
+async def on_ready():
+    global SERVER_GENERAL_CHANNEL
+    for server in client.guilds:
+        for channel in server.channels:
+            if(channel.name == 'general'):
+                SERVER_GENERAL_CHANNEL=client.get_channel(SERVER_GENERAL_CHANNEL_ID)
+        for member in server.members:
+            print("members in the server",member);
+
+
+# if anyone post the message in the server then this willl triggger......
+@client.event 
+async def on_message(message):
+    if(message.content):
+        await which_command(message.content,message.author.name);
+        
+async def sendMessage():
+    if(not SERVER_GENERAL_CHANNEL):
+        return
+    try:
+        await SERVER_GENERAL_CHANNEL.send("bhen ke bhai")
+    except:
+        print("something went wrong!");
+        return;
+                    
+
+client.run(TOKEN)
